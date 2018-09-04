f8aa6633ad684fb399e8a67b0648fb72109855b1
gabrielSBorges <32948255+gabrielSBorges@users.noreply.github.com>
2018-09-04 20:37:09 -0300

    Update ControlaJogo.js

--- htdocs/jogo/js/ControlaJogo.js
+++ htdocs/jogo/js/ControlaJogo.js
@@ -1,16 +1,1 @@
-(function(){
-    this.pronto;
-
-    window.addEventListener("keydown", function(e){
-	    pressionaTecla(e);
-    }, false);
-    
-    if (this.pronto == true){
-        cenaExploracao();
-    }
-    
-
-    function loop(){
-	    window.requestAnimationFrame(loop, cnv);
-	}
-})();
+window.onload = cenaExploracao();
