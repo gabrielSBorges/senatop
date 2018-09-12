363f166bea2552ecf372b682e0ba502c33deb260
gabrielSBorges <borgesgabrielsilva@gmail.com>
2018-09-12 19:45:20 -0300

    Arquivos das falas atualizados.

--- htdocs/jogo/js/falas/falasNPCs.js
+++ htdocs/jogo/js/falas/falasNPCs.js
@@ -4,154 +4,216 @@ this.mensagensNpcs[0] = {
 	[0] : "A vida de empreendedor não é fácil, eu preciso me esforçar muito para alcançar meu objetivo.",
 	[1] : "Por sorte existem instituições como essa que nos preparam para esse mundo...",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[1] = {
 	[0] : "Estou nesta instituição já fazem 2 longos anos, o tempo passa rápido e pelo visto eu aprendo",
 	[1] : "devagar. E você? É melhor não ficar de moleza.",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[2] = {
 	[0] : "É triste saber que está insvestindo seu dinheiro em algo que não está dando muito retorno.",
 	[1] : "Minhas aulas não estão sendo lá as melhores...",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[3] = {
 	[0] : "As experiências acumuladas demonstram que o aumento do diálogo entre os diferentes setores",
 	[1] : "produtivos afeta positivamente  a correta previsão dos conhecimentos estratégicos atingindo  a",
 	[2] : "excelência.",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[4] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[5] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[6] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[7] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[8] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[9] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[10] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[11] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[12] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[13] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[14] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[15] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[16] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[17] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[18] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[19] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[20] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[21] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[22] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[23] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[24] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[25] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[26] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[27] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[28] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[29] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
 this.mensagensNpcs[30] = {
 	[0] : "",
 	[1] : "",
 	[2] : "",
+	[3] : "",
+	[4] : "",
 };
